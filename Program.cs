b433d1dc5639c354dad2b3de47418243d2811853
krasikov <5aget@mail.ru>
2024-04-02 12:40:54 +0400

    Task-01: Added all files of project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using GitClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace GitClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Square square = new Square();
+
+            Console.Write("Введите радиус окружности  ");
+            double R = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите сторону квадрата  ");
+            double A = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Площадь окружности - {circle.CalculateAreaCircle(R)}");
+            Console.WriteLine($"Периметр окружности - {circle.CalculatePerimetrCircle(R)}");
+
+            Console.WriteLine($"Площадь квадрата - {square.CalculateAreaSquare(A)}");
+            Console.WriteLine($"Периметр квадрата - {square.CalculatePerimetrSquare(A)}");
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitClass
+{
+    public class Square
+    {
+        public double CalculateAreaSquare(double A)
+        {
+            double result =  Math.Pow(A, 2);
+            return result;
+        }
+
+        public double CalculatePerimetrSquare(double A)
+        {
+            double result =  A * 4;
+            return result;
+        }
+    }
+}
